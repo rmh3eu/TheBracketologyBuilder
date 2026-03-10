01199d8de72cf6b26e215e6531f33ceb3e30d1ef
rmh3eu <rmh3eu@virginia.edu>
2026-03-10 01:53:37 -0400

    Add files via upload

---
+++ projectionBases.js
@@ -0,0 +1,272 @@
+export const CURRENT_BASE_VERSION = 2;
+
+export const PROJECTION_BASES = {
+  2: {
+  "version": 2,
+  "created": "base_2_team_update",
+  "east": [
+    {
+      "seed": 1,
+      "team": "Duke"
+    },
+    {
+      "seed": 2,
+      "team": "Michigan St"
+    },
+    {
+      "seed": 3,
+      "team": "Virginia"
+    },
+    {
+      "seed": 4,
+      "team": "St Johns"
+    },
+    {
+      "seed": 5,
+      "team": "Arkansas"
+    },
+    {
+      "seed": 6,
+      "team": "Wisconsin"
+    },
+    {
+      "seed": 7,
+      "team": "Tennessee"
+    },
+    {
+      "seed": 8,
+      "team": "Saint Louis"
+    },
+    {
+      "seed": 9,
+      "team": "Kentucky"
+    },
+    {
+      "seed": 10,
+      "team": "UCF"
+    },
+    {
+      "seed": 11,
+      "team": "Santa Clara"
+    },
+    {
+      "seed": 12,
+      "team": "High Point"
+    },
+    {
+      "seed": 13,
+      "team": "Liberty"
+    },
+    {
+      "seed": 14,
+      "team": "Tennessee St"
+    },
+    {
+      "seed": 15,
+      "team": "Merrimack"
+    },
+    {
+      "seed": 16,
+      "team": "LIU"
+    }
+  ],
+  "west": [
+    {
+      "seed": 1,
+      "team": "Arizona"
+    },
+    {
+      "seed": 2,
+      "team": "Iowa State"
+    },
+    {
+      "seed": 3,
+      "team": "Gonzaga"
+    },
+    {
+      "seed": 4,
+      "team": "Kansas"
+    },
+    {
+      "seed": 5,
+      "team": "Purdue"
+    },
+    {
+      "seed": 6,
+      "team": "Saint Mary’s"
+    },
+    {
+      "seed": 7,
+      "team": "Miami FLA"
+    },
+    {
+      "seed": 8,
+      "team": "Villanova"
+    },
+    {
+      "seed": 9,
+      "team": "Ohio State"
+    },
+    {
+      "seed": 10,
+      "team": "Texas"
+    },
+    {
+      "seed": 11,
+      "team": "USF"
+    },
+    {
+      "seed": 12,
+      "team": "Utah Valley"
+    },
+    {
+      "seed": 13,
+      "team": "N Dakota St"
+    },
+    {
+      "seed": 14,
+      "team": "Portland St"
+    },
+    {
+      "seed": 15,
+      "team": "Wright St"
+    },
+    {
+      "seed": 16,
+      "team": "Queens"
+    }
+  ],
+  "midwest": [
+    {
+      "seed": 1,
+      "team": "Michigan"
+    },
+    {
+      "seed": 2,
+      "team": "UConn"
+    },
+    {
+      "seed": 3,
+      "team": "Nebraska"
+    },
+    {
+      "seed": 4,
+      "team": "Alabama"
+    },
+    {
+      "seed": 5,
+      "team": "North Carolina"
+    },
+    {
+      "seed": 6,
+      "team": "Miami Ohio"
+    },
+    {
+      "seed": 7,
+      "team": "TCU"
+    },
+    {
+      "seed": 8,
+      "team": "Georgia"
+    },
+    {
+      "seed": 9,
+      "team": "Clemson"
+    },
+    {
+      "seed": 10,
+      "team": "Iowa"
+    },
+    {
+      "seed": 11,
+      "team": "Missouri"
+    },
+    {
+      "seed": 12,
+      "team": "Yale"
+    },
+    {
+      "seed": 13,
+      "team": "SF Austin"
+    },
+    {
+      "seed": 14,
+      "team": "Troy"
+    },
+    {
+      "seed": 15,
+      "team": "Furman"
+    },
+    {
+      "seed": 16,
+      "team": "Howard"
+    }
+  ],
+  "south": [
+    {
+      "seed": 1,
+      "team": "Florida"
+    },
+    {
+      "seed": 2,
+      "team": "Houston"
+    },
+    {
+      "seed": 3,
+      "team": "Illinois"
+    },
+    {
+      "seed": 4,
+      "team": "Texas Tech"
+    },
+    {
+      "seed": 5,
+      "team": "Vanderbilt"
+    },
+    {
+      "seed": 6,
+      "team": "Louisville"
+    },
+    {
+      "seed": 7,
+      "team": "BYU"
+    },
+    {
+      "seed": 8,
+      "team": "UCLA"
+    },
+    {
+      "seed": 9,
+      "team": "Utah State"
+    },
+    {
+      "seed": 10,
+      "team": "NC State"
+    },
+    {
+      "seed": 11,
+      "team": "Texas A&M"
+    },
+    {
+      "seed": 12,
+      "team": "N. Iowa"
+    },
+    {
+      "seed": 13,
+      "team": "Hofstra"
+    },
+    {
+      "seed": 14,
+      "team": "UC Irvine"
+    },
+    {
+      "seed": 15,
+      "team": "Boston U"
+    },
+    {
+      "seed": 16,
+      "team": "UMBC"
+    }
+  ]
+}
+};
