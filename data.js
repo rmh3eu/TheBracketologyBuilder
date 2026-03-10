c836e735c65260e0a98cf188480be4612d2dd786
rmh3eu <rmh3eu@virginia.edu>
2026-03-10 03:14:32 -0400

    Add files via upload

--- data.js
+++ data.js
@@ -98,10 +98,84 @@ const PROJECTION_BASES = {
       [15, "Portland State"],
       [16, "UMBC"]
     ],
+  },
+  2: {
+    EAST: [
+      [1, "Duke"],
+      [2, "Michigan St"],
+      [3, "Virginia"],
+      [4, "St Johns"],
+      [5, "Arkansas"],
+      [6, "Wisconsin"],
+      [7, "Saint Mary’s"],
+      [8, "Saint Louis"],
+      [9, "Kentucky"],
+      [10, "UCF"],
+      [11, "Santa Clara"],
+      [12, "High Point"],
+      [13, "Liberty"],
+      [14, "Tennessee St"],
+      [15, "Merrimack"],
+      [16, "LIU"]
+    ],
+    WEST: [
+      [1, "Arizona"],
+      [2, "Iowa State"],
+      [3, "Gonzaga"],
+      [4, "Kansas"],
+      [5, "Purdue"],
+      [6, "Tennessee"],
+      [7, "Miami FLA"],
+      [8, "Villanova"],
+      [9, "Ohio State"],
+      [10, "Texas"],
+      [11, "USF"],
+      [12, "Utah Valley"],
+      [13, "N Dakota St"],
+      [14, "Portland St"],
+      [15, "Wright St"],
+      [16, "Queens"]
+    ],
+    MIDWEST: [
+      [1, "Michigan"],
+      [2, "UConn"],
+      [3, "Nebraska"],
+      [4, "Alabama"],
+      [5, "North Carolina"],
+      [6, "Miami Ohio"],
+      [7, "TCU"],
+      [8, "Georgia"],
+      [9, "Clemson"],
+      [10, "Iowa"],
+      [11, "Missouri"],
+      [12, "Yale"],
+      [13, "SF Austin"],
+      [14, "Troy"],
+      [15, "Furman"],
+      [16, "Howard"]
+    ],
+    SOUTH: [
+      [1, "Florida"],
+      [2, "Houston"],
+      [3, "Illinois"],
+      [4, "Texas Tech"],
+      [5, "Vanderbilt"],
+      [6, "Louisville"],
+      [7, "BYU"],
+      [8, "UCLA"],
+      [9, "Utah State"],
+      [10, "NC State"],
+      [11, "Texas A&M"],
+      [12, "N. Iowa"],
+      [13, "Hofstra"],
+      [14, "UC Irvine"],
+      [15, "Boston U"],
+      [16, "UMBC"]
+    ],
   }
 };
 
-const CURRENT_BASE_VERSION = 1;
+const CURRENT_BASE_VERSION = 2;
 
 function getCurrentProjectionBase() {
   return PROJECTION_BASES[CURRENT_BASE_VERSION];
