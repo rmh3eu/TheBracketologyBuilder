c2ceaef727a461cd5cd58e111d585d211d91e3b0
rmh3eu <rmh3eu@virginia.edu>
2026-03-16 21:04:28 -0400

    Add files via upload

--- functions/api/featured.js
+++ functions/api/featured.js
@@ -2,19 +2,7 @@ import { json } from "./_util.js";
 
 export async function onRequestGet({ env }){
   const rs = await env.DB.prepare(
-    `SELECT fr.bracket_id, fr.caption, fr.approved_at, b.title,
-            EXISTS(
-              SELECT 1 FROM challenge_entries ce
-               WHERE ce.bracket_id = fr.bracket_id
-                 AND LOWER(ce.challenge) = 'best'
-               LIMIT 1
-            ) AS entered_best,
-            EXISTS(
-              SELECT 1 FROM challenge_entries ce
-               WHERE ce.bracket_id = fr.bracket_id
-                 AND LOWER(ce.challenge) = 'worst'
-               LIMIT 1
-            ) AS entered_worst
+    `SELECT fr.bracket_id, fr.caption, fr.approved_at, b.title
      FROM feature_requests fr
      JOIN brackets b ON b.id = fr.bracket_id
      WHERE fr.status='approved'
