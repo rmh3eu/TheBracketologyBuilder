de26fd868fd4d6253d1dea31c347f1a2ad00167a
rmh3eu <rmh3eu@virginia.edu>
2026-03-31 01:26:05 -0400

    Add files via upload

--- functions/api/_leaderboard_second_static.js
+++ functions/api/_leaderboard_second_static.js
@@ -1,18 +1,8 @@
 export const SECOND_CHANCE_META = {
-  "games_played": 8
+  "games_played": 12
 };
 
 export const SECOND_CHANCE_RESULTS = [
-  {
-    "id": "REGION_WEST__R2__G0",
-    "winner": "Arizona",
-    "loser": "Arkansas"
-  },
-  {
-    "id": "REGION_WEST__R2__G1",
-    "winner": "Purdue",
-    "loser": "Texas/NC State"
-  },
   {
     "id": "REGION_SOUTH__R2__G0",
     "winner": "Iowa",
@@ -22,6 +12,16 @@ export const SECOND_CHANCE_RESULTS = [
     "id": "REGION_SOUTH__R2__G1",
     "winner": "Illinois",
     "loser": "Houston"
+  },
+  {
+    "id": "REGION_WEST__R2__G0",
+    "winner": "Arizona",
+    "loser": "Arkansas"
+  },
+  {
+    "id": "REGION_WEST__R2__G1",
+    "winner": "Purdue",
+    "loser": "Texas/NC State"
   },
   {
     "id": "REGION_EAST__R2__G0",
@@ -42,5 +42,25 @@ export const SECOND_CHANCE_RESULTS = [
     "id": "REGION_MIDWEST__R2__G1",
     "winner": "Tennessee",
     "loser": "Iowa St"
+  },
+  {
+    "id": "REGION_SOUTH__R3__G0",
+    "winner": "Illinois",
+    "loser": "Iowa"
+  },
+  {
+    "id": "REGION_WEST__R3__G0",
+    "winner": "Arizona",
+    "loser": "Purdue"
+  },
+  {
+    "id": "REGION_EAST__R3__G0",
+    "winner": "UConn",
+    "loser": "Duke"
+  },
+  {
+    "id": "REGION_MIDWEST__R3__G0",
+    "winner": "Michigan",
+    "loser": "Tennessee"
   }
 ];
